395c99800db1da1f007e3d15fc18ef88969b8a40
Christian H Hannah <60013267+christianhannah@users.noreply.github.com>
2024-08-06 12:06:28 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -13,11 +13,11 @@ setup(
     version='0.1.0',                    # Replace with your project's version
     packages=find_packages(),           # Automatically find packages in the project
     install_requires=[                  # List your project's dependencies here
-        'numpy>=1.26.4',
-        'scipy>=1.13.0<1.15.0',
-        'astropy>=5.3.4',
-        'tqdm>=4.66.2',
-        'mgefit>=5.0.15'
+        'numpy',
+        'scipy',
+        'astropy',
+        'tqdm',
+        'mgefit'
     ],
     url='https://github.com/christianhhannah/reptide',  # Replace with your GitHub URL
     author='Christian H. Hannah',                 # Replace with your name
@@ -27,7 +27,6 @@ setup(
     long_description_content_type='text/markdown',
     classifiers=[
         'Programming Language :: Python :: 3',
-        'License :: OSI Approved :: MIT License',  # Replace with your license
         'Operating System :: OS Independent',
     ],
     python_requires='>=3.11.7',  # Specify your Python version requirements
